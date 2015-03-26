bdfdde66330ba56366b07ac4daf0d5d1ef1cca49
amendiI <raphaelmuller@hotmail.fr>
2015-03-26 15:33:32 +0100

    ré assemblage de l'initialisation et mise en place du mélange

--- bataille/main.c
+++ bataille/main.c
@@ -1,5 +1,5 @@
-//#include "graphics.h"
-#include <stdio.h> //au cas ou on a pas le graphics.c
+#include "graphics.h"
+//#include <stdio.h> //au cas ou on a pas le graphics.c
 typedef struct carte {
 	
 	int coul; //1:coeur / 2:carreau / 3:pique / 4:trèfle
@@ -8,27 +8,42 @@ typedef struct carte {
 } carte;
 
 
-void melange(carte jeu[52]) //initialise le jeu en le mélangant
+void tri(carte jeu[52]) //initialise le jeu en le mélangant
 {
-	int a,b,c;
+	int a,b;
 	int ID=1;
 	
-	for(a=1;a<13;a++)
+	for(a=1;a<=13;a++)
 	{
-		for(b=0;b<4;b++)
+		for(b=1;b<=4;b++)
 		{
 			
-			c=alea_int(52);
-			
-			if(jeu[c].valeur==0)
-			{
-				jeu[c].valeur=a;
-				jeu[c].coul=b;
-				jeu[c].ID=ID;
-				ID++; 
-			}
+			jeu[ID].valeur=a;
+			jeu[ID].coul=b;
+			jeu[ID].ID=ID;
+			ID++; 
 		}
 	}
+}
+
+void melange(carte jeu[52],carte remplacent[52])
+{
+	int c,v,a;
+	
+	for(c=1;c<=52;c++)
+	{
+		v=0;
+		do{
+			a=alea_int(52);
+			
+			if(remplacent[a].ID==0)
+			{
+				remplacent[a]=jeu[c];
+				v=1;
+			}
+		}while(v==0);
+	}
+	
 }
 
 void init_paquet(carte jeu[52])
@@ -45,10 +60,13 @@ void init_paquet(carte jeu[52])
 
 void init_jeu(carte jeu1[52],carte jeu2[52]) //initialise le jeu(inutile?)
 {
+	carte jeu3[52];
+	
 	init_paquet(jeu1);
 	init_paquet(jeu2);
-	melange(jeu1);
-	
+	init_paquet(jeu3);
+	tri(jeu3);
+	melange(jeu3,jeu1);	
 }
 
 int comparaison(carte jeu1[52],carte jeu2[52])//comapare les 2 premières cartes de chaque paquet et return la valeur du gagnant
@@ -81,10 +99,14 @@ int main ()
 		p1=nb_de_carte(jeu1);
 		p2=nb_de_carte(jeu2);
 	}
-	for (a=0;a<52;a++)
+	for (a=0;a<=52;a++)
 	{
-		printf("%d   %d  %d \n",jeu1[a].valeur,jeu1[a].coul,a); 
+		printf("%d   %d   %d  \n",jeu1[a].ID,jeu1[a].valeur,jeu1[a].coul); 
 	}
 	
 	exit(0);
 }
+
+//faire une pile inversée
+//élément du dessu
+//elément du dessou
